444b51fdd27ba30cee5d86c31269d12d44da1fc3
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-02-03 13:33:05 -0500

    Initial Commit

---
+++ Layouts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Layouts
+//
+//  Created by Noah McGuire on 2/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by Noah McGuire on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
